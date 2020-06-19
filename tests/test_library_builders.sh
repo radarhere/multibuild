f2cbe413619b895f348a6adae19ad8c7f65e6527
Andrew Murray <radarhere@users.noreply.github.com>
2020-06-19 17:28:17 +1000

    back

--- tests/test_library_builders.sh
+++ tests/test_library_builders.sh
@@ -7,7 +7,7 @@ if [ -n "$IS_OSX" ]; then
     # Building on macOS
     export BUILD_PREFIX="${PWD}/builds"
     rm_mkdir $BUILD_PREFIX
-    # configure_build.sh sourced in test_multibuild.sh
+    source configure_build.sh
     source library_builders.sh
 else
     # Building on Linux
@@ -27,27 +27,13 @@ start_spinner
 # Run here just for the output, even though they fail.
 echo "TORCH -1"
 (set +e ;
-    build_github fredrik-johansson/arb 2.17.0 ;
+    echo "TORCH GITHUB LIBPNG";
     build_github glennrp/libpng v1.6.37 ;
-    build_github wbhart/mpir mpir-3.0.0
+    echo "TORCH GITHUB LIBPNG END";
     )
 echo "TORCH 7"
-suppress build_freetype
+build_freetype
 echo "TORCH 8"
-suppress build_libyaml
-if [ -z "$IS_OSX" ]; then
-    # Gives compiler conformance error on macOS Sierra:
-    # https://gist.github.com/5e20e137ea51fa8ca9fc443191f9d463
-    # https://gist.github.com/ad86c474f3c0b7ec74290bb13f9414af
-    suppress build_lzo
-fi
-suppress build_ragel
-if [ -z "$IS_OSX" ]; then
-    # already installed in the macOS image, so `brew install cfitsio` fails
-    suppress build_cfitsio
-fi
-suppress build_new_zlib
-suppress build_hdf5
 
 [ ${MB_PYTHON_VERSION+x} ] || ingest "\$MB_PYTHON_VERSION is not set"
 [ "$MB_PYTHON_VERSION" == "$PYTHON_VERSION" ] || ingest "\$MB_PYTHON_VERSION must be equal to \$PYTHON_VERSION"
